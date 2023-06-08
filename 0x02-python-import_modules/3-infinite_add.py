3041287d1f98185b0e436dd2d0c79d6fd835bbfb
Benard Ronoh <biikate48@gmail.com>
2023-06-08 07:26:09 -0700

    fixed pycodestyle requirements on 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,14 +2,15 @@
 # 3-infinite_add.py
 import sys
 
+
 def infinite_add(args):
     total = 0
     for arg in args:
         total += int(arg)
     return total
 
+
 if __name__ == "__main__":
     args = sys.argv[1:]
     result = infinite_add(args)
     print(result)
-
