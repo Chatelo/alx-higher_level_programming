7f8680eef321fcd870a036a60ca0b017d5acf647
Benard Ronoh <biikate48@gmail.com>
2023-06-26 02:27:13 -0700

    5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def raise_exception_msg(message=""):
-    """Raise a NameError exception with a custom message."""
-    raise NameError(message)
+def raise_exception():
+    """Raise a TypeError exception."""
+    raise TypeError
 
