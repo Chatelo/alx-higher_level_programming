4908765d3a43d1441eddde748997e574fb4df5f7
Benard Ronoh <biikate48@gmail.com>
2023-06-27 08:12:12 -0700

    103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""This module defines a class MagicClass."""
 
 import math
 
 
 class MagicClass:
+    """A class MagicClass that represents a circle."""
 
     def __init__(self, radius=0):
         """Initialize a MagicClass instance with a given radius."""
@@ -12,6 +14,7 @@ class MagicClass:
         self.__radius = float(radius)
 
     def area(self):
+        """Return the area of the circle."""
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
