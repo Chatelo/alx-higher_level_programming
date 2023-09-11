08c0b4b6df33f5ee56d40073f82e646e8d87b849
Benard Ronoh <biikate48@gmail.com>
2023-09-11 16:04:39 +0300

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,4 +3,6 @@
 const c = 'C is fun';
 const python = 'Python is cool';
 const javaScript = 'JavaScript is amazing';
-console.log(c, python, javaScript);
+console.log(c);
+console.log(python);
+console.log(javaScript);
