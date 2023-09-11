9b239452b891c6c2fb5c2b862356e1093d431ab1
Benard Ronoh <biikate48@gmail.com>
2023-09-11 16:13:49 +0300

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
+const args = process.argv.length - 2;
+
 if (args.length === 0) {
 	console.log('No argument');
 } else if (args.length === 1) {
