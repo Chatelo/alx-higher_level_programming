b46a1675656ce56aa24019cb036d07241d42ea30
Benard Ronoh <biikate48@gmail.com>
2023-09-12 13:18:32 +0300

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,35 +2,26 @@
 
 class Rectangle {
 	constructor(w, h) {
-		if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-			// Create an empty object if w or h is not a positive integer
-			this.width = undefined;
-			this.height = undefined;
-		} else {
-			// Initialize the instance attributes with the provided values
-			this.width = w;
-			this.height = h;
+		if (
+			typeof w !== 'number' ||
+			typeof h !== 'number' ||
+			w <= 0 ||
+			h <= 0 ||
+			!Number.isInteger(w) ||
+			!Number.isInteger(h)
+		) {
+			// Create an empty object if the provided values are not valid
+			return {};
 		}
+
+		this.width = w;
+		this.height = h;
 	}
 
 	print() {
-		if (this.width === undefined || this.height === undefined) {
-			console.log('Invalid rectangle');
-		} else {
-			for (let i = 0; i < this.height; i++) {
-				let row = '';
-				for (let j = 0; j < this.width; j++) {
-					row += 'X';
-				}
-				console.log(row);
-			}
+		for (let i = 0; i < this.height; i++) {
+			console.log('X'.repeat(this.width));
 		}
 	}
 }
-
-// Example usage:
-const validRectangle = new Rectangle(5, 3);
-validRectangle.print();
-
-const invalidRectangle = new Rectangle(0, 4);
-invalidRectangle.print();
+module.exports = Rectangle;
