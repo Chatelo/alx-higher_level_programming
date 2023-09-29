e3a0cd4b85bb1f54d02450866736ec9a8b5983d6
Benard Ronoh <biikate48@gmail.com>
2023-09-29 12:52:41 +0300

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
 Sends a POST request to a given URL with a given email.
-
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
+Usage: ./6-post_email.py <URL> <email> Displays the body of the response.
 """
 import sys
 import requests
