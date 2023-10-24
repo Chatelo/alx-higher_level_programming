2dc0fcf9545501bb97ffc5fe2c8b1c01c0bebf39
Benard Ronoh <biikate48@gmail.com>
2023-10-24 17:50:56 +0300

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Get the file path and content from command-line arguments
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Write the content to the file asynchronously in utf-8 encoding
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    // If an error occurred during writing, print the error object
+    console.error(err);
+  }
+});
