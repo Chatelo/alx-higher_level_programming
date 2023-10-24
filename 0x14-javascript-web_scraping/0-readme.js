018c3ca8354e6f707d15a00cced7731159241a18
Benard Ronoh <biikate48@gmail.com>
2023-10-24 17:49:38 +0300

    0-readme.js ES^ fails

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@
 
 // Import the required Node.js modules
 const argv = process.argv; // The command-line arguments array
-import { readFile } from 'fs'; // File system module to read files
+const fs = require('fs'); // File system module to read files
 
 // Read the content of the file asynchronously in utf-8 encoding
-readFile(argv[2], 'utf8', function (err, data) {
+fs.readFile(argv[2], 'utf8', function (err, data) {
 	if (err) {
 		// If an error occurred during reading, print the error object
 		console.log(err);
